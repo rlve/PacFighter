7f1dc2a48426740e964f25e43ea7d1b1c9cd19ca
rlve <radioslav@gmail.com>
2017-10-23 22:12:45 +0200

    Add: Creating walls - initial

---
+++ Assets/Scripts/Dungeon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dungeon : MonoBehaviour {
+    GameObject brick;
+
+	// Use this for initialization
+	void Start () {
+        brick = Resources.Load("brick_border") as GameObject;
+
+
+	}
+	
+    void MakeWall()
+    {
+
+    }
+
+}
